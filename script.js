59b00baf0b1b4cd9d7b47db7aae2f9734cccb71d
Andrew <andrewsabatyuk17@gmail.com>
2020-08-07 11:12:59 +0300

    init

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function showData (data, isDisplayed){
+    if (isDisplayed){
+        console.log(data);
+    }
+}
+
+function Car (brandName, model, productionYear){
+    this.brandName = brandName;
+    this.model = model;
+    this.productionYear = productionYear;
+}
+
+let createObj = _.create(Car.prototype, {
+    brandName:'Mercedes-Benz',
+    model:'C220',
+    productionYear: 2014
+})
+showData(createObj, true);
+
+let invertObj = _.invert(createObj);
+showData(invertObj, true);
+
+let pickObj = _.pick(createObj, 'brandName');
+showData(pickObj, true);
+
+let omitObj = _.omit(createObj, 'model');
+showData(omitObj, true);
